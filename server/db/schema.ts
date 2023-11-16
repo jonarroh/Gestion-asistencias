f7b89fb5e06db6c62068c0bc0a95e5762fc72d42
jonarroh <jonarrodi99@gmail.com>
2023-11-16 14:47:12 -0600

    🐛 fix (server) Schemas de bd

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,89 +1,115 @@
-import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
+import {
+	sqliteTable,
+	text,
+	integer,
+	real
+} from 'drizzle-orm/sqlite-core';
 
-type status = "activo" | "inactivo";
-type role = "alumno" | "docente" | "directivo" | "padre" | "escolares";
+type status = 'activo' | 'inactivo';
+type role =
+	| 'alumno'
+	| 'docente'
+	| 'directivo'
+	| 'padre'
+	| 'escolares';
 
-export const persona = sqliteTable("persona", {
-	clave: integer("clave").primaryKey(),
-	nombre: text("nombre"),
-	apellidoMaterno: text("apellidoMaterno"),
-	apellidoPaterno: text("apellidoPaterno"),
-	estatus: text("estatus")
-		.$default(() => "activo")
+export const persona = sqliteTable('persona', {
+	clave: integer('clave').primaryKey(),
+	nombre: text('nombre'),
+	apellidoMaterno: text('apellidoMaterno'),
+	apellidoPaterno: text('apellidoPaterno'),
+	estatus: text('estatus')
+		.$default(() => 'activo')
 		.$type<status>(),
-	role: text("role")
-		.$default(() => "alumno")
-		.$type<role>(),
+	role: text('role')
+		.$default(() => 'alumno')
+		.$type<role>()
 });
 
-export const asistencia = sqliteTable("asistencia", {
-	clave: integer("clave").primaryKey(),
-	fecha: text("fecha"),
-	asistencia: text("asistencia", { mode: "json" }),
-	clave_persona: integer("clave_persona").references(() => persona.clave),
+export const asistencia = sqliteTable('asistencia', {
+	clave: integer('clave').primaryKey(),
+	fecha: text('fecha'),
+	asistencia: text('asistencia', { mode: 'json' }),
+	clave_persona: integer('clave_persona').references(
+		() => persona.clave
+	)
 });
 
-export const materia = sqliteTable("materia", {
-	clave: integer("clave").primaryKey(),
-	nombre: text("nombre"),
-	periodo: text("periodo"),
-	clave_especialidad: text("clave_especialidad").references(
-		() => especialidad.clave,
+export const materia = sqliteTable('materia', {
+	clave: integer('clave').primaryKey(),
+	nombre: text('nombre'),
+	clave_periodo: integer('clave_periodo').references(
+		() => periodo.clave
 	),
+	clave_especialidad: integer('clave_especialidad').references(
+		() => especialidad.clave
+	)
 });
 
-export const alumno = sqliteTable("alumno", {
-	clave: integer("clave").primaryKey(),
-	matricula: text("matricula"),
-	materia: text("materia"),
-	clave_materia: text("clave_materia"),
-	periodo: text("periodo"),
-	grupo: text("grupo"),
-	clave_persona: integer("clave_persona").references(() => persona.clave),
+export const alumno = sqliteTable('alumno', {
+	clave: integer('clave').primaryKey(),
+	matricula: text('matricula'),
+	materia: text('materia'),
+	clave_materia: text('clave_materia'),
+	periodo: text('periodo'),
+	grupo: text('grupo'),
+	clave_persona: integer('clave_persona').references(
+		() => persona.clave
+	)
 });
 
-export const padre = sqliteTable("padre", {
-	clave: integer("clave").primaryKey(),
-	clave_persona: integer("clave_persona").references(() => persona.clave),
-	clave_alumno: integer("clave_alumno").references(() => alumno.clave),
-});
-
-export const directivo = sqliteTable("directivo", {
-	clave: integer("clave").primaryKey(),
-	clave_persona: integer("clave_persona").references(() => persona.clave),
-});
-
-export const docente = sqliteTable("docente", {
-	clave: integer("clave").primaryKey(),
-	clave_persona: integer("clave_persona").references(() => persona.clave),
-});
-
-export const especialidad = sqliteTable("especialidad", {
-	clave: integer("clave").primaryKey(),
-	nombre: text("nombre"),
-});
-
-export const periodo = sqliteTable("periodo", {
-	clave: integer("clave").primaryKey(),
-	nombre: text("nombre"),
-	fecha_inicio: text("fecha_inicio").notNull(),
-	fecha_fin: text("fecha_fin").notNull(),
-});
-
-export const grupo = sqliteTable("grupo", {
-	clave: integer("clave").primaryKey(),
-	nombre: text("nombre"),
-	clave_especialidad: text("clave_especialidad").references(
-		() => especialidad.clave,
+export const padre = sqliteTable('padre', {
+	clave: integer('clave').primaryKey(),
+	clave_persona: integer('clave_persona').references(
+		() => persona.clave
 	),
-	id_alumno: integer("id_alumno").references(() => alumno.clave),
-	id_maestro: integer("id_maestro").references(() => docente.clave),
-	periodo: text("periodo").references(() => materia.periodo),
+	clave_alumno: integer('clave_alumno').references(() => alumno.clave)
 });
 
-export const materias_grupo = sqliteTable("materias_grupo", {
-	clave: integer("clave").primaryKey(),
-	clave_materia: integer("clave_materia").references(() => materia.clave),
-	clave_grupo: integer("clave_grupo").references(() => grupo.clave),
-	horas: real("horas"),
+export const directivo = sqliteTable('directivo', {
+	clave: integer('clave').primaryKey(),
+	clave_persona: integer('clave_persona').references(
+		() => persona.clave
+	)
+});
+
+export const docente = sqliteTable('docente', {
+	clave: integer('clave').primaryKey(),
+	clave_persona: integer('clave_persona').references(
+		() => persona.clave
+	)
+});
+
+export const especialidad = sqliteTable('especialidad', {
+	clave: integer('clave').primaryKey(),
+	nombre: text('nombre')
+});
+
+export const periodo = sqliteTable('periodo', {
+	clave: integer('clave').primaryKey(),
+	nombre: text('nombre'),
+	fecha_inicio: text('fecha_inicio').notNull(),
+	fecha_fin: text('fecha_fin').notNull()
+});
+
+export const grupo = sqliteTable('grupo', {
+	clave: integer('clave').primaryKey(),
+	nombre: text('nombre'),
+	clave_especialidad: integer('clave_especialidad').references(
+		() => especialidad.clave
+	),
+	id_alumno: integer('id_alumno').references(() => alumno.clave),
+	id_maestro: integer('id_maestro').references(() => docente.clave),
+	clave_periodo: integer('clave_periodo').references(
+		() => periodo.clave
+	)
+});
+
+export const materias_grupo = sqliteTable('materias_grupo', {
+	clave: integer('clave').primaryKey(),
+	clave_materia: integer('clave_materia').references(
+		() => materia.clave
+	),
+	clave_grupo: integer('clave_grupo').references(() => grupo.clave),
+	horas: real('horas')
 });
--- server/db/seed.ts
+++ server/db/seed.ts
@@ -1,108 +1,105 @@
-import { db } from "./db";
-import * as schema from "./schema";
+import { db } from './db';
+import * as schema from './schema';
 
 // Insertar personas en la tabla 'persona'
 await db.insert(schema.persona).values([
 	{
-		nombre: "Jorge",
-		apellidoMaterno: "Gonzalez",
-		apellidoPaterno: "Perez",
-		estatus: "activo",
-		role: "alumno",
+		nombre: 'Jorge',
+		apellidoMaterno: 'Gonzalez',
+		apellidoPaterno: 'Perez',
+		estatus: 'activo',
+		role: 'alumno'
 	},
 	{
-		nombre: "Maria",
-		apellidoMaterno: "Rodriguez",
-		apellidoPaterno: "Lopez",
-		estatus: "activo",
-		role: "docente",
+		nombre: 'Maria',
+		apellidoMaterno: 'Rodriguez',
+		apellidoPaterno: 'Lopez',
+		estatus: 'activo',
+		role: 'docente'
 	},
 	{
-		nombre: "Pedro",
-		apellidoMaterno: "Martinez",
-		apellidoPaterno: "Gomez",
-		estatus: "activo",
-		role: "directivo",
+		nombre: 'Pedro',
+		apellidoMaterno: 'Martinez',
+		apellidoPaterno: 'Gomez',
+		estatus: 'activo',
+		role: 'directivo'
 	},
 	{
-		nombre: "Ana",
-		apellidoMaterno: "Hernandez",
-		apellidoPaterno: "Diaz",
-		estatus: "activo",
-		role: "padre",
+		nombre: 'Ana',
+		apellidoMaterno: 'Hernandez',
+		apellidoPaterno: 'Diaz',
+		estatus: 'activo',
+		role: 'padre'
 	},
 	{
-		nombre: "Carlos",
-		apellidoMaterno: "Fernandez",
-		apellidoPaterno: "Gutierrez",
-		estatus: "activo",
-		role: "escolares",
-	},
+		nombre: 'Carlos',
+		apellidoMaterno: 'Fernandez',
+		apellidoPaterno: 'Gutierrez',
+		estatus: 'activo',
+		role: 'escolares'
+	}
 ]);
 
 // Insertar en la tabla 'asistencia'
 await db.insert(schema.asistencia).values({
-	fecha: "2023-11-16",
+	fecha: '2023-11-16',
 	asistencia: '{"asistencia": "presente"}',
-	clave_persona: 1,
+	clave_persona: 1
 });
 
 // Insertar en la tabla 'alumno'
 await db.insert(schema.alumno).values({
-	matricula: "12345",
-	materia: "Matemáticas",
-	clave_materia: "Materia1",
-	periodo: "Periodo1",
-	grupo: "Grupo1",
-	clave_persona: 1,
+	matricula: '12345',
+	materia: 'Matemáticas',
+	clave_materia: 'Materia1',
+	periodo: 'Periodo1',
+	grupo: 'Grupo1',
+	clave_persona: 1
 });
 
 // Insertar en la tabla 'padre'
 await db.insert(schema.padre).values({
 	clave_persona: 4,
-	clave_alumno: 1,
+	clave_alumno: 1
 });
 
 // Insertar en la tabla 'directivo'
 await db.insert(schema.directivo).values({
-	clave_persona: 3,
+	clave_persona: 3
 });
 
 // Insertar en la tabla 'docente'
 await db.insert(schema.docente).values({
-	clave_persona: 2,
+	clave_persona: 2
 });
 
 // Insertar en la tabla 'materias'
 await db.insert(schema.materia).values({
-	nombre: "Física",
-	periodo: "Periodo1",
-	clave_especialidad: "Especialidad1",
+	clave_especialidad: 1,
+	clave_periodo: 1,
+	nombre: 'Matemáticas'
 });
 
 // Insertar en la tabla 'especialidad'
 await db.insert(schema.especialidad).values({
-	nombre: "Ingeniería",
+	nombre: 'Ingeniería'
 });
 
 // Insertar en la tabla 'periodo'
 await db.insert(schema.periodo).values({
-	nombre: "Semestre 1",
-	fecha_inicio: "2023-01-01",
-	fecha_fin: "2023-06-30",
+	nombre: 'Semestre 1',
+	fecha_inicio: '2023-01-01',
+	fecha_fin: '2023-06-30'
 });
 
 // Insertar en la tabla 'grupo'
 await db.insert(schema.grupo).values({
-	nombre: "Grupo1",
-	clave_especialidad: "Especialidad1",
-	id_alumno: 1,
-	id_maestro: 1,
-	periodo: "Periodo1",
+	nombre: 'Grupo 1',
+	clave_periodo: 1
 });
 
 // Insertar en la tabla 'materias_grupo'
 await db.insert(schema.materias_grupo).values({
 	clave_grupo: 1,
-	horas: 3.5,
+	horas: 3.5
 });
