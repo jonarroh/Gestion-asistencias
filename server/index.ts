50abb322ad516967e8f94f999a173aa30277aa40
jonarroh <jonarrodi99@gmail.com>
2023-11-16 21:45:26 -0600

    🐛 fix (galleto) SelectGalleta

--- server/index.ts
+++ server/index.ts
@@ -1,40 +1,40 @@
-import { Elysia } from 'elysia';
-import { swagger } from '@elysiajs/swagger';
-import { jwt } from '@elysiajs/jwt';
-import { cookie } from '@elysiajs/cookie';
-import { authApp } from './routes/auth';
-import { cors } from '@elysiajs/cors';
+import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { jwt } from "@elysiajs/jwt";
+import { cookie } from "@elysiajs/cookie";
+import { authApp } from "./routes/auth";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
 	.use(cors())
-	.use(swagger({ path: '/docs' }))
+	.use(swagger({ path: "/docs" }))
 	.use(
 		jwt({
-			name: 'jwt',
+			name: "jwt",
 			secret: Bun.env
 				.SUPER_SECRET_VALUE_DONT_SHARE_OR_SOMEONE_WILL_BE_FIRED as string,
-			exp: '7d'
-		})
+			exp: "7d",
+		}),
 	)
 	.use(cookie())
 	.use(authApp)
-	.get('/home', async ({ jwt, set, cookie: { auth } }) => {
+	.get("/home", async ({ jwt, set, cookie: { auth } }) => {
 		const profile = await jwt.verify(auth);
 		if (!profile) {
 			set.status = 401;
 			return {
-				message: 'Unauthorized'
+				message: "Unauthorized",
 			};
 		}
 		return {
-			message: 'Hello World from home'
+			message: "Hello World from home",
 		};
 	})
-	.get('/', () => {
+	.get("/", () => {
 		return {
-			message: 'Hello World'
+			message: "Hello World",
 		};
 	})
 	.listen(3000);
 
-console.log('Server is running on port 3000');
+console.log("Server is running on port 3000");
