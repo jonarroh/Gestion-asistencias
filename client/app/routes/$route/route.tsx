9d9dd1182657a5a76c4d8c14cdb3376f6438c1cc
jonarroh <jonarrodi99@gmail.com>
2023-11-20 21:44:57 -0600

    ✨ feat (client) Agregado 404 page

--- client/app/routes/$route/route.tsx
+++ client/app/routes/$route/route.tsx
@@ -1,12 +1,19 @@
 import { LoaderFunctionArgs, json, redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import {
+	useLoaderData,
+	useRouteError,
+	isRouteErrorResponse,
+	Link
+} from '@remix-run/react';
 import { role, roles } from '~/types';
+import Layout from '~/Layout/Login';
+import Footer from '~/components/login/Footer';
+import { Card, CardContent, CardHeader } from '~/components/ui/card';
 
 export const loader = async ({
 	params,
 	request
 }: LoaderFunctionArgs) => {
-	console.log(params);
 	const cookiesHeader = request.headers.get('Cookie');
 
 	// Parsear las cookies si están presentes
@@ -32,8 +39,48 @@ export const loader = async ({
 	return json({ route: params.route });
 };
 
+export function ErrorBoundary() {
+	const caught = useRouteError();
+
+	if (isRouteErrorResponse(caught)) {
+		return (
+			<>
+				<Layout>
+					<section className="flex flex-col items-center justify-center w-full h-full ">
+						<Card className="w-[400px] h-[400px] md:w-[575px] md:h-[438px]  justify-center flex-row ">
+							<CardHeader>
+								<img
+									src="/logo_utl.png"
+									alt="logo"
+									className="self-center"
+									width={84.12}
+									height={100}
+								/>
+							</CardHeader>
+							<CardContent>
+								<p className="text-2xl font-bold text-center text-gray-500 ">
+									{caught.status === 404
+										? 'Pagina no encontrada'
+										: 'Error desconocido'}
+								</p>
+								<p className="text-center ">
+									<Link to="/" className="mt-4 text-blue-500  ">
+										Regresar al inicio
+									</Link>
+								</p>
+							</CardContent>
+						</Card>
+					</section>
+				</Layout>
+				<Footer />
+			</>
+		);
+	}
+}
+
 function Route() {
 	const { route } = useLoaderData<typeof loader>();
+	const errors = useRouteError();
 
 	return (
 		<>
